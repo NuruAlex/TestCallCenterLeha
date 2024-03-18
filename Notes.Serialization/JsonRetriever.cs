bbe5de41d9e91a49ca38685ece373bc3520a98b1
NuruAlex <nurualeksej@gmail.com>
2024-03-18 14:07:21 +0300

    add collection

---
+++ Notes.Serialization/JsonRetriever.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Notes.Serialization
+{
+    public class JsonRetriever
+    {
+        public string Extension => ".json";
+
+        private readonly JsonSerializerSettings _serializeSettings = new()
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        public List<T> LoadFromFile<T>(string path) => JsonConvert.DeserializeObject<List<T>>(
+                value: File.ReadAllText(path),
+                settings: _serializeSettings) ?? [];
+
+        public void SaveToFile<T>(List<T> data, string path) => File.WriteAllText(
+                path: path,
+                contents: JsonConvert.SerializeObject(data, _serializeSettings));
+    }
+}
---
+++ Notes.Serialization/UniqeCollection.cs
@@ -0,0 +1,76 @@
+﻿namespace Notes.Serialization
+{
+    public abstract class UniqeCollection<T>
+    {
+        private List<T> _list;
+      
+        private readonly string _path;
+       
+        public List<T> Items
+        {
+            get
+            {
+                return _list;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+
+
+
+        public UniqeCollection(string path)
+        {
+            _list = new JsonRetriever().LoadFromFile<T>(path);
+            _path = path;
+        }
+
+        public void Refresh()
+        {
+            new JsonRetriever().SaveToFile(_list, _path);
+            _list = new JsonRetriever().LoadFromFile<T>(_path);
+        }
+
+
+        public void Add(T item)
+        {
+            if (item == null || Contains(item))//не допускаем null и такой же элемент в коллекции
+            {
+                return;
+            }
+
+            _list.Add(item);
+            Refresh();
+        }
+        public void Delete(Predicate<T> match)
+        {
+            _list.RemoveAll(match);
+            Refresh();
+        }
+
+
+
+        public T? Find(Predicate<T> match)
+        {
+            return _list.Find(match);
+        }
+        public List<T> FindAll(Predicate<T> match)
+        {
+            return _list.FindAll(match);
+        }
+        public bool Contains(Predicate<T> match)
+        {
+            return Find(match) != null;
+        }
+
+
+        public abstract bool Contains(T item);
+        public abstract void Delete(T item);
+
+    }
+}
