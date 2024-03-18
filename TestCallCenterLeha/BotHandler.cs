f237b1467785ecabfe167e026dc01d707c22f0d6
NuruAlex <nurualeksej@gmail.com>
2024-03-18 12:01:33 +0300

    Короче бот добавлен

---
+++ TestCallCenterLeha/BotHandler.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TestCallCenterLeha;
+
+public static class BotHandler
+{
+    private static TelegramBotClient _bot;
+
+    public static void Initialize(string botToken)
+    {
+        _bot = new TelegramBotClient(botToken);
+        _bot.StartReceiving(OnUpdate, OnError);
+    }
+    private static async Task OnUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        //обрботка всех апдейтов
+    }
+
+    private static async Task OnError(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        //обработка ошибок от бота
+    }
+
+}
+
