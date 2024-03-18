9ccfd176da0075afce052ed6101831bc333a5a8b
NuruAlex <nurualeksej@gmail.com>
2024-03-18 12:24:53 +0300

    Добавили обработку текста

--- TestCallCenterLeha/BotHandler.cs
+++ TestCallCenterLeha/BotHandler.cs
@@ -12,9 +12,45 @@ public static class BotHandler
         _bot = new TelegramBotClient(botToken);
         _bot.StartReceiving(OnUpdate, OnError);
     }
+
     private static async Task OnUpdate(ITelegramBotClient client, Update update, CancellationToken token)
     {
-        //обрботка всех апдейтов
+        int offset = 0;
+
+        //глобально опрашиваем сервер нахуй
+        while (true)
+        {
+            //Получаем апдейты с сервера
+            Update[] updates = await _bot.GetUpdatesAsync(offset);
+
+            foreach (Update newUpdate in updates)//прогоняем все
+            {
+                offset = update.Id + 1;//чтобы в следующий раз мы с другим сдвигом брали обновления
+
+                await HandleUpdate(newUpdate);
+            }
+        }
+    }
+
+    private static async Task HandleUpdate(Update update)
+    {
+        if (update.Message != null)
+        {
+            //получаем сообщение из апдейта
+            Message message = update.Message;
+
+            if (message.Text == null)
+            {
+                return;
+            }
+
+            if (message.Text == "/start")
+            {
+                await _bot.SendTextMessageAsync(
+                    chatId: message.Chat.Id,//берем чат с которого нам написали
+                    text: "Привет всем");
+            }
+        }
     }
 
     private static async Task OnError(ITelegramBotClient client, Exception exception, CancellationToken token)
