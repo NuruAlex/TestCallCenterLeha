ab5a75b7061a0ec262f1c42487e00c562397a4a1
Sinitsyn Alexander <shurik120304@yandex.ru>
2024-03-18 16:04:59 +0300

    Added data type for Note

---
+++ Notes.Core/Note.cs
@@ -0,0 +1,24 @@
+﻿namespace Notes.Core
+{
+    public class Note
+    {
+        public Note(int NoteId, int NoteChatId, string NoteText)
+        {
+            Id = NoteId;
+            ChatId = NoteChatId;
+            Text = NoteText;
+            CreateDate = DateTime.Now;
+        }
+
+        private string _Text = "";
+
+        public readonly int Id;
+
+        public readonly int ChatId;
+        public string Text { get { return _Text; } set { _Text = value; } }
+
+        public readonly DateTime CreateDate;
+        public override string ToString() => $"{Id.ToString()}\n{ChatId.ToString()}\n{Text}\n{CreateDate.ToString()}";
+    }
+
+}
